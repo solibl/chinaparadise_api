8b510281de140c90ee7fcee3fcade2bbdd4b6d63
Brian Lee <brianleedongjun@gmail.com>
2018-01-11 04:43:23 -0800

    Refactor schema to make ingredients not be required

--- db/migrate/20180109232127_create_menus.rb
+++ db/migrate/20180109232127_create_menus.rb
@@ -2,7 +2,7 @@ class CreateMenus < ActiveRecord::Migration[5.1]
   def change
     create_table :menus do |t|
     	t.string :name, null: false
-    	t.string :ingredients, null: false
+    	t.string :ingredients
     	t.money :price, :precision => 10, :scale => 2, null: false
     	t.integer :category, null: false
     	t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20180110065412) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ingredients", null: false
-    t.decimal "price", precision: 10, scale: 2, null: false
+    t.string "ingredients"
+    t.money "price", scale: 2, null: false
     t.integer "category", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
