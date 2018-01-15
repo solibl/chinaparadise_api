5ced20d132c01f658edcfa50c62aad388ce72a72
Brian Lee <brianleedongjun@gmail.com>
2018-01-14 16:13:15 -0800

    Refactor rack-cors

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module ChinaParadiseApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
         allow do
-            origins 'https://china-paradise.herokuapp.com/'
+            origins '*'
             resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options], expose: ['Access-Token', 'Uid']
         end
     end
